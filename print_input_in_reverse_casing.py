1c17a0a5990e294cbcb5c512d22f71facd4e49cc
unknown <janruzzellorquia88@gmail.com>
2025-03-29 12:52:47 +0800

    Correction on comment line 4

--- print_input_in_reverse_casing.py
+++ print_input_in_reverse_casing.py
@@ -1,12 +1,7 @@
-print_input_in_reverse_casing.py
 #ask user for their name
 name = input("Enter your full name in incorrect casing: ")
 
-#print input with capital letters format
-for letter in name:
-    if letter .isupper():
-        letter.lower()
-    else:
-        letter.upper()
 
-print(letter)
+#print input with reverse casing
+print("Your name is: " + name.swapcase())
+
