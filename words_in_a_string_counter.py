e0a60b91827e94e0152beb12ec68bb4fef7ecc39
unknown <janruzzellorquia88@gmail.com>
2025-03-29 12:58:34 +0800

    words in a string counter using len() and split() function

---
+++ words_in_a_string_counter.py
@@ -0,0 +1,6 @@
+#aks user for a complete sentence 
+sentence = input("Enter your sentence: ")
+
+#count the number of words using len() function and split() function
+word_counter = len(sentence.split())
+print(word_counter)
